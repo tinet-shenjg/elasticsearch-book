e8e1f8e0eee565a39e8e4d6567d8750965625114
申建刚 <shenjg@aliyun.com>
2019-07-09 18:03:12 +0800

    push

--- src/main/java/com/shenjg/book/confingration/ElasticsearchConfig.java
+++ src/main/java/com/shenjg/book/confingration/ElasticsearchConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * ElasticsearchConfig
+ * Elasticsearch配置类
  *
  * @author shenjg
  * @date 2019/07/09
