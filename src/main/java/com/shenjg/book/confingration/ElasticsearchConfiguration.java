12a72210f42bf2fb20b622c8dbe92b1ad3d2b13b
申建刚 <shenjg@aliyun.com>
2019-07-09 16:00:18 +0800

    init

---
+++ src/main/java/com/shenjg/book/confingration/ElasticsearchConfiguration.java
@@ -0,0 +1,10 @@
+package com.shenjg.book.confingration;
+
+/**
+ * ElasticsearchConfiguration
+ *
+ * @author shenjg
+ * @date 2019/07/09
+ */
+public class ElasticsearchConfiguration {
+}
---
+++ src/test/java/com/shenjg/book/ElasticsearchBookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shenjg.book;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchBookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
