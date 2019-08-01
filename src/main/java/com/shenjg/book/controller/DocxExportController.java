c68c87a50a5178d88d68e5d4bb1315d39e4b6514
申建刚 <jgshen@foxmail.com>
2019-08-01 20:39:12 +0800

    “”

--- src/main/java/com/shenjg/book/controller/DocxExportController.java
+++ src/main/java/com/shenjg/book/controller/DocxExportController.java
@@ -5,8 +5,11 @@ import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -21,14 +24,18 @@ import java.util.Objects;
 public class DocxExportController {
 
     @RequestMapping("title")
-    public void title() {
+    public void title(HttpServletResponse response) {
         XWPFTemplate template = null;
+        String url = "docTemplates"+ File.separator+ "template_title.docx";
         try {
-            template = XWPFTemplate.compile("~/docTemplates/template_title.docx").render(new HashMap<String, Object>() {{
-                put("title", "Poi-tl 模板引擎");
-            }});
-            FileOutputStream out = new FileOutputStream("out_template.docx");
+            template = XWPFTemplate.compile(this.getClass().getClassLoader()
+                    .getResourceAsStream(url))
+                    .render(new HashMap<String, Object>() {{
+                        put("title", "Poi-tl 模板引擎");
+                    }});
+            OutputStream out = response.getOutputStream();;
             template.write(out);
+            out.flush();
         } catch (Exception e) {
 
         } finally {
