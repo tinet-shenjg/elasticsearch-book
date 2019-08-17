aef04208308d5a29f31b4919c76fad386bc8f573
tinet-shenjg <jgshen@foxmail.com>
2019-08-17 23:33:20 +0800

    no message

--- src/main/java/com/shenjg/book/service/impl/AdminUserServiceImpl.java
+++ src/main/java/com/shenjg/book/service/impl/AdminUserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.shenjg.book.service.impl;
 
 import com.shenjg.book.entity.AdminUser;
+import com.shenjg.book.mapper.AdminUserMapper;
 import com.shenjg.book.service.AdminUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +16,16 @@ import java.util.List;
  */
 @Service
 public class AdminUserServiceImpl implements AdminUserService {
+
+    @Autowired
+    private AdminUserMapper adminUserMapper;
     @Override
     public List<AdminUser> list() {
-        return null;
+        return adminUserMapper.selectList(null);
+    }
+
+    @Override
+    public Integer add(AdminUser adminUser) {
+        return adminUserMapper.insert(adminUser);
     }
 }
